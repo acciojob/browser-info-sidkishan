d3e9affd2fb4928f6d2be5870ded4dbda4ce3790
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-05-18 23:41:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+var browserName = navigator.userAgent.split('/',1)[0];
+var version = navigator.userAgent;
+document.getElementById('browser-info').innerText = "You are using " + browserName + " version " + version;
